3e0309d3b29222193578e3afa6fa49e8f8d13e75
Roshan Chand <rosanchand1234@gmail.com>
2024-07-14 18:06:36 +0545

    made-cart-increment-using-select

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -61,6 +61,8 @@ document.querySelectorAll('.js-add-to-cart')
     button.addEventListener('click',()=>{
         const productId = button.dataset.productId;
 
+        let incrementValue = document.querySelector(`.js-quantity-selector-${productId}`).value;
+        // console.log(incrementValue);
         let matchingItem;
         cart.forEach((item)=>{
             if(productId === item.productId){
@@ -70,12 +72,12 @@ document.querySelectorAll('.js-add-to-cart')
         });
 
         if(matchingItem){
-            matchingItem.quantity +=1
+            matchingItem.quantity += parseInt(`${incrementValue}`);
         }
         else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: parseInt(`${incrementValue}`)
             });
         }
 
